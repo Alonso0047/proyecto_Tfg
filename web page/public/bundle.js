91e9ae8f078c26cd79b6b461fd76636bf4030dca
alonso <alonsorayo007@gmail.com>
2024-05-20 19:06:36 +0200

    cambiando codigo

--- web page/public/bundle.js
+++ web page/public/bundle.js
@@ -659,3 +659,30 @@ ventanaCarrito.addEventListener('click', (e)=>{
 ventanaCarrito.querySelector('#carrito__btn-comprar').addEventListener('click', ()=>{
     
 });
+
+class Tabs{
+    constructor(idElemento){
+        //Creamos una variable para la pestaña
+         this.tabs = document.getElementById(idElemento);
+         //Creamos otra variable para los botones de navegacion
+         this.nav = this.tabs.querySelector('.tabs');
+         this.nav.addEventListener('click', (e)=>{
+           if([...e.target.classList].includes('tabs__button')){
+            const tab = e.target.dataset.tab;
+
+            if(this.tabs.querySelector('.tab--active')){
+                this.tabs.querySelector('.tab--active').classList.remove('tab--active');
+            }
+
+            if(this.tabs.querySelector('.tabs__button--active')){
+                this.tabs.querySelector('.tabs__button--active').classList.remove('tabs__button--active');
+            }
+
+            this.tabs.querySelector(`#${tab}`).classList.add('tab--active');
+            e.target.classList.add('tabs__button--active');
+           } 
+         });
+    }
+}
+
+new Tabs('mas-informacion');
